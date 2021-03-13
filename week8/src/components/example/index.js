d809d5ddcb32c1fb5311221f60e0bce2b5e9529b
Ipugh29 <77811339+Ipugh29@users.noreply.github.com>
2021-03-12 22:34:16 -0500

    Add files via upload

---
+++ week8/src/components/example/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Example extends React.Component {
+
+    render() {
+        return <p style={{ color: "white" }}>My name is Isaiah</p>;
+    }
+}
+
+export default Example;
