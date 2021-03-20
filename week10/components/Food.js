bbede7f46b2d43aa51c449289d4107a64835ab11
Ipugh29 <77811339+Ipugh29@users.noreply.github.com>
2021-03-19 23:43:31 -0400

    Add files via upload

---
+++ week10/components/Food.js
@@ -0,0 +1,12 @@
+import React, { Component } from "React"
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Food extends Component {
+    render() {
+        return (
+            <view>
+                <text>French Fries</text>
+            </view>
+        )
+    }
+}
